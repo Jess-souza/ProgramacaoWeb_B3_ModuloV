077a46b724aca1aa41f255afab7235e2ba3d8301
Jessica <jsouzasouza120@gmail.com>
2023-01-08 18:32:31 -0300

    Initial commit

---
+++ src/test/java/br/com/ada/adaflix/AdaflixApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.ada.adaflix;
+
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
+class AdaflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/br/com/ada/adaflix/SpringBootTest.java
@@ -0,0 +1,4 @@
+package br.com.ada.adaflix;
+
+public @interface SpringBootTest {
+}
